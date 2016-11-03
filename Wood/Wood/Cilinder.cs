238a63ac62dfe8fcf6545c166edd01235f588783
dima <dima2012@outlook.com>
2016-11-03 18:57:28 +0200

    Wood step #1

---
+++ Wood/Wood/Cilinder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Wood
+{
+    class Cilinder
+    {
+        protected float R1 = 0,R2 = 0,L = 0;
+
+        public Cilinder()
+        {
+            
+        }
+
+        /*
+         * возращает коды ошибок
+         * 1 - R1 <= 0
+         * 2 - R2 <= 0
+         * 3 - L <= 0
+        */
+        public virtual int Set(float R1,float R2,float L)
+        {
+            if (R1 <= 0)
+                return 1;
+            if (R2 <= 0)
+                return 2;
+            if (L <= 0)
+                return 3;
+
+            if (R1 < R2)
+            {
+                this.R1 = R1;
+                this.R2 = R2;
+            }
+            else
+            {
+                this.R1 = R2;
+                this.R2 = R1;
+            }
+            this.L = L;
+
+            return 0;
+        }
+
+        public virtual float V()
+        {
+            float value = (float)Math.PI * R2 * R2 * L;
+            return value;
+        }
+    }
+}
---
+++ Wood/Wood/Conus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wood
+{
+    class Conus:Cilinder
+    {
+        private float H = 0;
+
+        public Conus()
+        {
+
+        }
+
+        /*
+         *4 - L < (R2 - R1) / 2
+        */
+        public override int Set(float R1, float R2, float L)
+        {
+            int value = base.Set(R1, R2, L);
+
+            if (value == 0)
+            {
+
+            }
+            else
+                return value;
+
+            if (L < ((R2 - R1) / 2))
+                return 4;
+
+            this.H = (float) Math.Sqrt((float)(Math.Pow(L, 2) - Math.Pow((R2 - R1) / 2, 2)));
+            return 0;
+        }
+
+        public override float V()
+        {
+            float value = (float)(Math.PI * H * (Math.Pow(R1, 2) + Math.Pow(R2, 2) + R2 * R1)) / 3;
+
+            return value;
+        }
+    }
+}
